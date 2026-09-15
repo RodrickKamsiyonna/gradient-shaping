5b951c9765baee1ba448d4918440ef59079bd7fd
Kamsi Rodrick <148686197+RodrickKamsiyonna@users.noreply.github.com>
2026-09-15 10:28:33 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -42,7 +42,7 @@ def _patched_init_action(self, n_envs, actions=None):
         device=self.device,
         dtype=self.dtype,
     )
-    * 0.01)
+    * 0.1)
     remaining = self.horizon - actions.shape[2]
 
     if remaining > 0:
